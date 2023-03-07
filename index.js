3580a445f56d93db90ef6931c9c301cc65442a15
Terbeche Mustapha <termus96@gmail.com>
2023-03-07 14:19:58 +0100

    Create .gitignore

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const http = require("http");
+const https = require("https");
 const express = require("express");
+const fs = require("fs");
 const app = express();
 app.use((request, response) => {
   response.write("<h1>Hello World!</h1>");
   response.end();
 });
-const server = http.createServer(app);
-server.listen(3000);
+const options = {
+  key: fs.readFileSync("server.key"),
+  cert: fs.readFileSync("server.cert"),
+};
+https.createServer(options, app).listen(3000);
